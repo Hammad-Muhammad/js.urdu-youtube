1632f5b896c35733755e68fd1de438e9f72b1399
Muhammad Hamad <mhamadkhanofficial@gmail.com>
2025-11-12 10:56:13 +0500

    conversion of datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+
+//datatype conversion and opperations
+let score = true
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number (score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33" => 33
+//"33abc" => NaN
+//true => 1; false => 0
+
+ let isLoggedin  = ""
+
+ let booleanIsLoggedin = Boolean(isLoggedin)
+console.log (booleanIsLoggedin);
+
+// 1 => true; 0 => false;
+// "" => false
+// "muhammad" => true
+
+let someNumber = 33
+
+let stringNumber = String (someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
