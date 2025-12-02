c0ff6311f21ed3a78981bfde71e90ffc2d121ce8
Muhammad Hamad <mhamadkhanofficial@gmail.com>
2025-12-02 16:33:05 +0500

    01arrays.js

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,27 @@
+//array
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeors = ["muhammad", "Abubakar"]
+
+const myArr2 = new Array (1, 2, 3, 4)
+console.log(myArr[1]);
+
+
+// Array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.shift() 
+// console.log(myArr); 
+
+// console.log(myArr.includes(5));
+// console.log(myArr.indexOf(3));
+
+
+// slice, splice
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1, 3)
