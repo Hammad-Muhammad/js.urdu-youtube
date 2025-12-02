900d21be282e6effe72b96200fd82c39e762e28d
Muhammad Hamad <mhamadkhanofficial@gmail.com>
2025-12-02 11:54:11 +0500

    datesinJs

---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,17 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+let myCreatedDate = new Date (2025, 11, 11);
+// let myCreatedDate = new Date (2025, 11, 11, 11, 20);
+// let myCreatedDate = new Date ("2025-12-11");
+// let myCreatedDate = new Date ("12-11-2025");
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
